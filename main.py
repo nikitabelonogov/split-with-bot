6a06faef7f2d4a8f726a71773bda24f269073b57
Nikita Belonogov <belonogovna@sgu.ru>
2018-09-20 11:41:18 +0400

    Add Debts class

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ from telegram.ext import Updater, CommandHandler
 import logging
 
 
+class Debt(object):
+    def __init__(self, lender, debtor, name, sum, interim_amount):
+        self.lender = lender
+        self.debtor = debtor
+        self.name = name
+        # TODO: DO NOT STORE MONEY IN FLOAT!
+        self.sum = float(sum)
+        self.interim_amount = float(interim_amount)
+        self.date = datetime.datetime.now()
+
+    def __str__(self):
+        return '{} {} lent {} {:.0f}₽ for {:.0f}₽ {}'.format(
+            self.date.date(), self.lender, self.debtor, self.interim_amount, self.sum, self.name)
+
+    def __int__(self):
+        return self.interim_amount
+
+
 def parse_mentions(message):
     result = []
     mentions = [x for x in message['entities'] if x['type'] == 'mention']
@@ -13,40 +31,64 @@ def parse_mentions(message):
     return result
 
 
-def lend(lender, debtors, name, sum, self_exept=False):
+def lend(lender, debtors, name, sum, self_except=False):
     response = ''
-    if not self_exept:
+    if not self_except:
         debtors.append(lender)
     debtors = set(debtors)
     interim_amount = sum / len(debtors)
     for debtor in debtors:
-        entry = {'lender': lender,
-                 'debtor': debtor,
-                 'name': name,
-                 'sum': sum,
-                 'interim_amount': interim_amount,
-                 'date': datetime.datetime.now(), }
-        DEBTS.append(entry)
-        response += '{} lent {} {:.0f}₽ for {:.0f}₽ {}\n'.format(lender, debtor, interim_amount, sum, name)
+        debt = Debt(lender, debtor, name, sum, interim_amount)
+        DEBTS.append(debt)
+        if not debt.lender == debt.debtor:
+            response += str(debt) + '\n'
     return response
 
 
 def lend_command(bot, update, args):
     lender = '@' + update.message.from_user.username
     name = args[0]
-    sum = int(args[1])
+    sum = float(args[1])
     debtors = parse_mentions(update.message)
-    responce = lend(lender, debtors, name, sum, self_exept=False)
-    update.message.reply_text(responce)
+    response = lend(lender, debtors, name, sum, self_except=False)
+    update.message.reply_text(response)
 
 
 def lend_self_except_command(bot, update, args):
     lender = '@' + update.message.from_user.username
     name = args[0]
-    sum = int(args[1])
+    sum = float(args[1])
     debtors = parse_mentions(update.message)
-    responce = lend(lender, debtors, name, sum, self_exept=True)
-    update.message.reply_text(responce)
+    response = lend(lender, debtors, name, sum, self_except=True)
+    update.message.reply_text(response)
+
+
+def history_command(bot, update, args):
+    username = '@' + update.message.from_user.username
+    response = ''
+    for debt in DEBTS:
+        if not debt.lender == debt.debtor:
+            if debt.lender == username or debt.debtor == username:
+                response += str(debt) + '\n'
+    update.message.reply_text(response)
+
+
+def status_command(bot, update, args):
+    response = ''
+    username = '@' + update.message.from_user.username
+    totals = {}
+    for debt in DEBTS:
+        if not debt.lender == debt.debtor:
+            if debt.lender == username:
+                totals[debt.debtor] = totals.get(debt.debtor, 0.) + int(debt)
+            elif debt.debtor == username:
+                totals[debt.lender] = totals.get(debt.debtor, 0.) - int(debt)
+    for username, total in totals.items():
+        if total < 0:
+            response += 'you owes {} {:.0f}₽ in total\n'.format(username, total)
+        elif total > 0:
+            response += 'you lent {} {:.0f}₽ in total\n'.format(username, total)
+    update.message.reply_text(response)
 
 
 def error_callback(bot, update, error):
@@ -67,6 +109,10 @@ if __name__ == '__main__':
 
     updater.dispatcher.add_handler(CommandHandler('lend', lend_command, pass_args=True))
     updater.dispatcher.add_handler(CommandHandler('lend_self_except', lend_self_except_command, pass_args=True))
+    updater.dispatcher.add_handler(CommandHandler('add', lend_command, pass_args=True))
+    updater.dispatcher.add_handler(CommandHandler('add_self_except', lend_self_except_command, pass_args=True))
+    updater.dispatcher.add_handler(CommandHandler('history', history_command, pass_args=True))
+    updater.dispatcher.add_handler(CommandHandler('status', status_command, pass_args=True))
     updater.dispatcher.add_error_handler(error_callback)
 
     updater.start_polling()
