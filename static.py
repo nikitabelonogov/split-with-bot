10b50d95a0a3cdd4249a99cfbb05dea93ad65c1c
Nikita Belonogov <waksaone@gmail.com>
2022-12-18 18:04:49 +0400

    Fix error message

--- static.py
+++ static.py
@@ -33,4 +33,4 @@ example: <code>/debt 1</code>
 debt_button_add_myself_to_debtors_text = "Add myself to debtors"
 debt_button_remove_myself_from_debtors_text = "Remove myself from debtors"
 
-error_message = "ERROR! got to https://github.com/nikitabelonogov/split-with-bot and fix it yourself"
+error_message = "ERROR! go to https://github.com/nikitabelonogov/split-with-bot and fix it yourself"
